fdf942eec0dbcd83aa2cb167206fa345508ab3a1
githumbi <githumbi74@gmail.com>
2024-04-21 23:01:43 +0300

    adjusted profile pic

--- pages/_document.js
+++ pages/_document.js
@@ -18,92 +18,51 @@ class MyDocument extends Document {
             type="text/css"
             media="all"
           />
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-            type="text/css"
-            media="all"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-            type="text/css"
-            media="all"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"
-            type="text/css"
-            media="all"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/gh/githumbi/portfolio-site@main/public/assets/css/vendors/magnific-popup.css"
-            type="text/css"
-            media="all"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/gh/githumbi/portfolio-site@main/public/assets/css/vendors/splitting.css"
-            type="text/css"
-            media="all"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/gh/githumbi/portfolio-site@main/public/assets/css/vendors/swiper.css"
-            type="text/css"
-            media="all"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/gh/githumbi/portfolio-site@main/public/assets/css/main.css"
-            type="text/css"
-            media="all"
-          />
+          
 
           {/* <!-- CSS STYLES --> */}
-          {/* <link
+          <link
             rel="stylesheet"
             href="assets/css/vendors/bootstrap.css"
             type="text/css"
             media="all"
-          /> */}
-          {/* <link
+          />
+         <link
             rel="stylesheet"
             href="assets/fonts/font-awesome/css/font-awesome.css"
             type="text/css"
             media="all"
-          /> */}
-          {/* <link
+          /> 
+          <link
             rel="stylesheet"
             href="assets/css/vendors/magnific-popup.css"
             type="text/css"
             media="all"
-          /> */}
-          {/* <link
+          /> 
+        <link
             rel="stylesheet"
             href="assets/css/vendors/splitting.css"
             type="text/css"
             media="all"
-          /> */}
-          {/* <link
+          /> 
+          <link
             rel="stylesheet"
             href="assets/css/vendors/swiper.css"
             type="text/css"
             media="all"
-          /> */}
-          {/* <link
+          /> 
+         <link
             rel="stylesheet"
             href="assets/css/vendors/animate.css"
             type="text/css"
             media="all"
-          /> */}
-          {/* <link
+          /> 
+           <link
             rel="stylesheet"
             href="assets/css/main.css"
             type="text/css"
             media="all"
-          /> */}
+          />
 
           {/* <!-- Favicon --> */}
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
